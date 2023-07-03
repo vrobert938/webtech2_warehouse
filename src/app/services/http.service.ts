f0b90fcac37faadb943ea66c831f36ca89105c1c
Róbert Vékony <ver7mc@bosch.com>
2023-07-03 02:01:26 +0200

    item CRUD components

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -3,6 +3,7 @@ import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { TextResp } from '../models/TextResp';
 import { AuthToken } from 'src/models/AuthToken';
 import { User } from 'src/models/User';
+import { Item } from 'src/models/Items';
 
 @Injectable({
   providedIn: 'root'
@@ -15,12 +16,10 @@ export class HttpService {
     return this.http.post<any>('http://localhost:3800/login', loginRequestModel);
   }
 
-
   refreshToken(access_token: AuthToken) {
     let headers = new HttpHeaders();
     return this.http.post<any>('http://localhost:3800/refresh-token', access_token, {headers: headers});
   }
-
 
   getWorks(){
     return this.http.get<TextResp>('http://localhost:3800/works');
@@ -30,5 +29,24 @@ export class HttpService {
     return this.http.post<any>('http://localhost:3800/register', user);
   }
 
+  createItem(item: Item){
+    return this.http.post<any>('http://localhost:3800/createItem', item);
+  }
 
+  getItems(){
+    return this.http.get<any>('http://localhost:3800/getItems');
+  }
+
+  getItemDetails(){
+
+  }
+
+  updateItem(){
+
+  }
+
+  deleteItem(itemId: Item){
+    
+    return this.http.post<any>('http://localhost:3800/deleteItem', itemId);
+  }
 }
